849bedcc629c3c228a0f0c45b784f346d8ee9fc3
Julio Cartier <juliocartier@gmail.com>
2017-11-27 09:48:33 -0300

    Algumas alterações

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -3848,3 +3848,20 @@ return exports;
 
 }({},$,Popper));
 //# sourceMappingURL=bootstrap.js.map
+
+function scrollNav() {
+  $('.nav a').click(function(){  
+    //Toggle Class
+    $(".active").removeClass("active");      
+    $(this).closest('li').addClass("active");
+    var theClass = $(this).attr("class");
+    $('.'+theClass).parent('li').addClass('active');
+    //Animate
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 160
+    }, 400);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
